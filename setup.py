2fe5cb6deb5faf9b92f7f2156edd396c8c2bc8c9
Andrey-Shatsev <a.g.shatsev@gmail.com>
2023-04-28 12:00:10 +0300

    Изменил файл setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from os.path import join, dirname
 setup(
     name='MyPythonFinance',
     version='1.0',
-    packages=find_packages(),
+    packages=['Mdf_downloader'],
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
 )
